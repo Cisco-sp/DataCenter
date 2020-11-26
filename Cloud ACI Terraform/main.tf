31b0134c96f0771bfe238c22c4718d3e1e3557ee
Henrique Prado (heprado) <heprado@cisco.com>
2020-11-26 19:55:09 -0300

    basics

--- Cloud ACI Terraform/main.tf
+++ Cloud ACI Terraform/main.tf
@@ -1,6 +1,6 @@
 provider aws {
-    region = 
-    credentials = 
+    region = var.aws_region
+    share_credentials_file = "C:/Users/heprado/Documents/Github/DataCenter/Cloud ACI Terraform/aws_credentials"
     profile = "default"
 }
 
@@ -9,4 +9,9 @@ provider mso {
     username = var.mso_username
     password = var.mso_password
     url = var.mso_url
+}
+
+
+provider vsphere {
+    
 }
---
+++ Cloud ACI Terraform/variables.tf
@@ -0,0 +1,16 @@
+variable aws_region {
+
+}
+
+variable mso_url {
+
+}
+variable mso_username {
+    type = string
+    description = "Usúario para acessar o MSO"
+}
+
+variable mso_password {
+    type = string
+    description = "Senha para acessar o MSO"
+}
