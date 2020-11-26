68c38bcba0f0a40105cc1c683839c0657ffc4ec4
Henrique Prado (heprado) <heprado@cisco.com>
2020-11-26 20:20:43 -0300

    vSphere done i think

--- Cloud ACI Terraform/main.tf
+++ Cloud ACI Terraform/main.tf
@@ -13,5 +13,58 @@ provider mso {
 
 
 provider vsphere {
-    
+    user = var.vsphere_username
+    password = var.vsphere_password
+}
+
+data "vsphere_datacenter" "datacenter" {
+  name = var.vsphere_dc_name
+}
+
+data "vsphere_compute_cluster" "compute_cluster" {
+  name          = var.vsphere_cluster_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.vsphere_datastore_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = "public"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.vsphere_template_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "Itau-Teste"
+  compute_cluster =  data.vsphere_compute_cluster.compute_cluster.id
+  datastore_id     = data.vsphere_datastore.datastore.id}
+
+  num_cpus = 2
+  memory   = 4096
+  guest_id = data.vsphere_virtual_machine.template.guest_id
+
+  scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+
+  disk {
+    label            = "disk0"
+    size             = data.vsphere_virtual_machine.template.disks.0.size
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+  }
 }
--- Cloud ACI Terraform/variables.tf
+++ Cloud ACI Terraform/variables.tf
@@ -5,6 +5,9 @@ variable aws_region {
 variable mso_url {
 
 }
+
+
+
 variable mso_username {
     type = string
     description = "Usúario para acessar o MSO"
@@ -13,4 +16,30 @@ variable mso_username {
 variable mso_password {
     type = string
     description = "Senha para acessar o MSO"
+}
+
+variable vsphere_dc_name {
+
+}
+
+variable vsphere_cluster_name {
+
+}
+
+variable vsphere_datastore_name {
+    
+}
+
+variable vsphere_username {
+    type = string
+    description = "Usúario para acessar o vCenter"
+}
+
+variable vsphere_password {
+    type = string
+    description = "Senha para acessar o MSO"
+}
+
+variable vsphere_template_name {
+    
 }
