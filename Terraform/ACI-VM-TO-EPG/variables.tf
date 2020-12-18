a3f6f4c2d68923afffba4e9993008e39f78922a3
Tidebinder <lipnose1@gmail.com>
2020-12-18 01:39:40 -0300

    almost done

---
+++ Terraform/ACI-VM-TO-EPG/variables.tf
@@ -0,0 +1,22 @@
+variable vmm_domain_name {
+
+}
+
+variable prefix_name {
+
+}
+
+variable apic_user {
+    type = string
+    description = "Usuário do APIC"
+}
+
+variable apic_pass {
+    type = string
+    description = "Senha do APIC"
+    sensitive = true
+}
+
+variable apic_ip {
+
+}
