8d3a9d35a92e35c5b5c5d50debd78a15f45568dc
Tidebinder <lipnose1@gmail.com>
2021-02-18 17:57:53 -0300

    adding cloud aci configs

--- Terraform/CLOUD_ACI_PVT/providers.tf
+++ Terraform/CLOUD_ACI_PVT/providers.tf
@@ -1,3 +1,13 @@
+/*This is needed because the ACI provider is not in the Official Hashicorp Repository, so we need to specify the path to the provider*/
+terraform {
+  required_providers {
+    aci = {
+      source = "CiscoDevNet/aci"
+      version = "0.5.4"
+    }
+  }
+}
+
 /*You need to use the AWS provider, the cloud_apic module will inherit the provider from here. */
 provider aws {
     region = var.aws_region
@@ -6,3 +16,7 @@ provider aws {
 }
 
 /*You need to use the ACI Provider, the deploy_aci_network will inherit the provider from here. */
+
+provider aci {
+
+}
